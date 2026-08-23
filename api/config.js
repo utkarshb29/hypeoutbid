1c5ecabd05e82e459e86853e4d24bb7694f88c6b
utkarshb29 <65757859+utkarshb29@users.noreply.github.com>
2026-08-23 12:00:33 +0530

    Delete api/config.js

--- api/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.json({
-    supabaseUrl: process.env.SUPABASE_URL,
-    supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
-    paymentEnabled: !!(process.env.INSTAMOJO_API_KEY),
-  });
-};
